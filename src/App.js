1b1bc4ec425a4e1b9a28ba8f974515a2c0aba680
Phillip Shebel <phil.shebel@gmail.com>
2019-12-02 15:31:08 -0800

    feat: auth, picker

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Amplify, { Storage } from 'aws-amplify';
+import awsconfig from './aws-exports';
+import { withAuthenticator } from 'aws-amplify-react'; 
+import { PhotoPicker } from 'aws-amplify-react';
+
+Amplify.configure(
+  {
+    Storage: {
+      AWSS3: {
+        bucket: 'snif-images-repo'
+      }
+    }
+  }
+)
+console.log(awsconfig)
+Amplify.configure(awsconfig);
+
+let uploadToStorage = async pathToImageFile => {
+  try {
+    const response = await fetch('/Users/PS/Desktop/dog0.jpg')
+    
+    const blob = await response.blob()
+    
+    Storage.put('dog0.jpg', blob, {
+      contentType: 'image/jpeg',
+    })
+  } catch (err) {
+    console.log(err)
+  }
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <PhotoPicker onPick={data => uploadToStorage(data)} />
       </header>
     </div>
   );
 }
-
-export default App;
+export default withAuthenticator(App, true);
+// export default App;
