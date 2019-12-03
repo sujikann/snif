0f911ca631d0a1f73314e4a9a6535d44bc7bfba8
Phillip Shebel <phil.shebel@gmail.com>
2019-12-02 16:29:44 -0800

    input instead of picker

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Amplify, { Storage } from 'aws-amplify';
 import awsconfig from './aws-exports';
 import { withAuthenticator } from 'aws-amplify-react'; 
-import { PhotoPicker } from 'aws-amplify-react';
+// import { PhotoPicker } from 'aws-amplify-react';
 
 // Amplify.configure(
 //   {
@@ -17,25 +17,43 @@ import { PhotoPicker } from 'aws-amplify-react';
 // console.log(awsconfig)
 Amplify.configure(awsconfig);
 
-let uploadToStorage = async pathToImageFile => {
-  try {
-    const response = await fetch('/Users/PS/Desktop/dog0.jpg')
-    
-    const blob = await response.blob()
-    
-    Storage.put('dog0.jpg', blob, {
-      contentType: 'image/jpeg',
-    })
-  } catch (err) {
-    console.log(err)
-  }
+// let uploadToStorage = async image => {
+//   try {
+//     console.log(image)
+//     console.log(image.files[0])
+//     // console.log(pathToImageFile)
+//     // const response = await fetch(pathToImageFile.name)
+//     // console.log(response)
+//     // const blob = await image.blob()
+//     // console.log(blob)
+//     Storage.put(image.name, image, {
+//       contentType: 'image/jpeg',
+//     })
+//   } catch (err) {
+//     console.log(err)
+//   }
+// }
+
+let onChange = (e) => {
+  console.log(e)
+  const file = e.target.files[0];
+  console.log(file)
+  Storage.put(file.name, file, {
+      contentType: 'image/png'
+  })
+  .then (result => console.log(result))
+  .catch(err => console.log(err));
 }
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <PhotoPicker onPick={data => uploadToStorage(data)} />
+        {/* <PhotoPicker onPick={data => uploadToStorage(data)} /> */}
+        <input
+              type="file" accept='image/jpg'
+              onChange={(e) => onChange(e)}
+          />
       </header>
     </div>
   );
